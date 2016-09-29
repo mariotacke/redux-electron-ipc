70920eecc9e12b58c734b4a17de2c8a7c1c8aae9
Mario Tacke <mario.tacke@with.me>
2016-09-29 12:24:58 -0700

    (fix) Add Electron prebuilt package to dev dependencies in example app. Simplify example structure.

--- example/app/app.js
+++ example/app/app.js
@@ -1,11 +1,29 @@
 import { applyMiddleware, createStore } from 'redux';
-import createIpc from '../../';
-import { pingActionCreator, pongActionCreator } from './actions';
-import { exampleReducer } from './reducer';
+import createIpc, { send } from '../../';
+
+function exampleReducer (state = {}, action) {
+    switch (action.type) {
+        case 'IPC_PONG':
+            console.log('Pong', action); // eslint-disable-line no-console
+            return state;
+        default:
+            return state;
+    }
+}
+
+
+function pongActionCreator (event, arg1, arg2, arg3) {
+    return {
+        type: 'IPC_PONG',
+        arg1,
+        arg2,
+        arg3
+    };
+}
 
 const ipc = createIpc({
     'pong': pongActionCreator // eslint-disable-line quote-props
 });
 const store = createStore(exampleReducer, applyMiddleware(ipc));
 
-store.dispatch(pingActionCreator('redux', 'electron', 'ipc'));
+store.dispatch(send('ping', 'redux', 'electron', 'ipc'));
